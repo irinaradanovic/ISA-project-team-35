8efee5880ff5367de747ad36e2a63fc0be87d1f7
irinaradanovic <irina.radanovic5@gmail.com>
2026-02-11 21:35:23 +0100

    debuf, new nginx.conf

--- jutjubic/src/main/java/com/isa/jutjubic/security/jwt/JwtTokenProvider.java
+++ jutjubic/src/main/java/com/isa/jutjubic/security/jwt/JwtTokenProvider.java
@@ -3,17 +3,31 @@ package com.isa.jutjubic.security.jwt;
 import com.isa.jutjubic.security.model.UserPrincipal;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    private  Key key;
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    //private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private final long expirationMs = 3600000; // 1 sat
+
+    @PostConstruct
+    public void init() {
+        // Inicijalizujemo ključ koristeći tvoj fiksni string iz properties-a
+        // Ovo osigurava da je ključ ISTI i nakon restarta servera
+        byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(Authentication authentication) {
 
